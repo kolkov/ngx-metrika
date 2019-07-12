51ab0a177c182c90efb742182a0a3745baea25f0
Andy <a.kolkov@gmail.com>
2019-07-12 23:11:24 +0300

    Fix test for service create

--- projects/kolkov/ngx-metrika/src/lib/ngx-metrika.service.spec.ts
+++ projects/kolkov/ngx-metrika/src/lib/ngx-metrika.service.spec.ts
@@ -1,12 +1,29 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {NgxMetrikaService} from './ngx-metrika.service';
+import {HttpClientModule} from '@angular/common/http';
+import {YM_CONFIG} from './ym.token';
+import {NgxMetrikaConfig} from './interfaces';
+import {NavigationEnd, Router, RouterModule, Scroll} from '@angular/router';
+import {of} from 'rxjs';
 
 describe('NgxMetrikaService', () => {
 
+  const NgxConfig: NgxMetrikaConfig = {
+    id: 49142767
+  };
+
+  const mockRouter = {
+    navigate: (commands: any[]) => { Promise.resolve(true); },
+    events: of(new Scroll(new NavigationEnd(0, 'dummyUrl', 'dummyUrl'), [0, 0], 'dummyString'))
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [NgxMetrikaService]
+      imports: [ HttpClientModule],
+      providers: [NgxMetrikaService,
+        { provide: YM_CONFIG, useValue: NgxConfig },
+        { provide: Router, useValue: mockRouter }]
     });
   });
 
